cbec57f4acba69a4271f6f845fdbcab25f45d1d4
harbor <harbor@MacBookPro.lan>
2025-07-19 18:31:39 +0530

    first commit

--- src/server.js
+++ src/server.js
@@ -19,7 +19,8 @@ app.use(cors({
   origin: [
     'http://localhost:3001',
     'http://localhost:3000',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    "https://compliance-manager-frontend.onrender.com"
   ],
   credentials: true
 }));
